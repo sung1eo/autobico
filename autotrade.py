cdf4cf4c490ed5e53c7ee56998ed56dda03385c3
sung1eo91 <sung1eo91@gmail.com>
2021-06-01 04:56:58 +0000

    timezone 수정, 4시간 로직 정비

--- autotrade.py
+++ autotrade.py
@@ -5,6 +5,8 @@ import pyupbit
 import numpy as np
 import datetime
 import configparser
+import logging
+from pytz import timezone
 
 config = configparser.ConfigParser()
 config.read(r"config/config.ini")
@@ -12,24 +14,36 @@ config.read(r"config/config.ini")
 access_key = config['DEFAULT']['ACCESS_KEY'] 
 secret_key = config['DEFAULT']['SECRET_KEY'] 
 
+KST = timezone('Asia/Seoul')
+
 # 타겟 금액 계산
 def get_target_price(ticker, k):
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=4) 
-    #print(df)
-    last_mid = df.iloc[-4] # 12시간 전
+    #print('Reset target price')
+    df = pyupbit.get_ohlcv(ticker, interval="minute30", count=9) 
+    
+    latest_time = df.index[-1]
+    if latest_time.minute != 30:
+        df = df.iloc[:-1]
+
+    last = df.iloc[-8] # 12시간 전
     now = df.iloc[-1]
 
+    #print(df)
+
     now_open = now['open'] # 현재기준 가장 최근시간
-    last_mid_high = last_mid['high'] # -12시간 전 최고가
-    last_mid_low = last_mid['low'] # -12시간 전 최저가
-    target = now_open + (last_mid_high - last_mid_low) * k
+    last_high = last['high'] # 4시간 전 최저가
+    last_low = last['low'] # 4시간 전 최저가
+    target = now_open + (last_high - last_low) * k
     return target
 
 # 시작 시간 계산
 def get_start_time(ticker):
     df = pyupbit.get_ohlcv(ticker, interval="minute240", count=1)
     start_time = df.index[0]
-    print('start_time=',start_time,sep=' ')
+    #logger.info('start_time={}'.format(start_time))
+    # if start_time.minute != 30:
+    #     start_time = start_time + datetime.timedelta(minutes=30)
+    #print(start_time)
     return start_time
 
 # 60시간(4시간x15) 이동평균선 계산
@@ -41,37 +55,50 @@ def get_ma15(ticker):
 # 내 잔고조회 (Coin 갯수)
 def get_balance(ticker):
     balances = upbit.get_balances()
-    print(balances)
+    #print(balances)
     for b in balances:
         if b['currency'] == ticker[ticker.index('-')+1:]:
             if b['balance'] is not None:
-                return float(b['balance'])
+                return float(b['balance']), float(b['avg_buy_price'])
             else:
                 return 0
     return 0
 
-# 주문의 uuid 조회
-def get_order_uuid(ticker):
-    orders = upbit.get_order()
-    uuid = orders['uuid']
-    return uuid
+# # 주문의 uuid 조회
+# def get_order_uuid(ticker):
+#     orders = upbit.get_order(ticker)
+#     uuid = orders['uuid']
+#     return uuid
 
 # 현재 구매 가능한 가격 조회
 def get_current_price(ticker):
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
 def get_ror(k, ticker):
-    df = pyupbit.get_ohlcv(ticker, interval = 'minutes240', count=200)
-    c = [-1-(i*3) for i in range(0,50)]
-    df = df.iloc[c] # 12시간 간격으로 자름
+    #df = pyupbit.get_ohlcv(ticker, interval = 'minutes240', count=200)
+    df = pyupbit.get_ohlcv(ticker, interval = 'minutes30', count=800)
+
+    if df.index[-1].minute != 30:
+        df = df.iloc[:-1] # 시간 정각은 제외한다
     
+    c = [-1-(i*8) for i in range(0,100)]
+    df = df.iloc[c] # 4시간 간격으로 자름
+
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1) 
-    
-    fee = 0.0032
-    df['ror'] = np.where(df['high'] > df['target'],df['close'] / df['target'] - fee,1)
 
+    fee = 0.0032
+    df['ror'] = np.where(df['high'] > df['target'],df['close'] / df['target'] - fee, 1)
     ror = df['ror'].cumprod()[-2]
+
+    df['hpr'] = df['ror'].cumprod()
+    df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+
+    #print("MDD(%): ", df['dd'].max())
+    #print("누적수익률(fee고려):", ror, sep=' ')
+    
+    #print(df.tail(10))
+    #print(df.head(10))
 
     return ror
 
@@ -82,15 +109,27 @@ def find_k(ticker):
         r_list.append(ror)
     #print(r_list)
     k = (int(r_list.index(max(r_list)))+1)/10
-
-    print('k_value=',k,sep=' ')
+    logger.info('k_value={}'.format(k))
     return k
 
 
-#print(get_target_price(ticker=ticker_ETC, k=0.5))
+logger = logging.getLogger()
+
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# log 출력
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+# log를 파일에 출력
+file_handler = logging.FileHandler('my.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 upbit = pyupbit.Upbit(access_key, secret_key)
-print("autotrade start")
+logger.info("autotrade start")
 
 ticker_BTC = "KRW-BTC" #비트코인
 ticker_ETC = "KRW-ETC" #이더리움 클래식
@@ -101,42 +140,41 @@ ticker_KRW = "KRW-KRW" #원화
 # 자동매매 시작
 
 today_ticker = ticker_ETC
-
-start_time = get_start_time(ticker=today_ticker)
 k_value = find_k(ticker=today_ticker)
 
 while True:
     try:
-        now = datetime.datetime.now()
-        end_time = start_time + datetime.timedelta(hours=12)
-
-        coin_volume = 0
-
-        # 12시간 동안,
+        now = datetime.datetime.now(KST)
+        start_time = get_start_time(ticker=today_ticker) + datetime.timedelta(minutes=30)
+        end_time = start_time + datetime.timedelta(hours=4)
+        start_time, end_time = start_time.replace(tzinfo=KST), end_time.replace(tzinfo=KST)
+        # 4시간
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price(ticker=today_ticker, k=k_value)
             ma15 = get_ma15(ticker=today_ticker)
             current_price = get_current_price(ticker=today_ticker)
-            print(now)
-            print('구매 목표가:{}//이동 평균선(60시간):{:.1f}//현재 금액:{}'.format(target_price, ma15, current_price))
-
+            logger.info('구매 목표가:{}//이동 평균선(60시간):{:.1f}//현재 금액:{}'.format(target_price, ma15, current_price))
             if target_price < current_price and ma15 < current_price:
+                logger.info('Meet the condition - buy')
+                #logger.info('구매 목표가:{}//이동 평균선(60시간):{:.1f}//현재 금액:{}'.format(target_price, ma15, current_price))
                 krw = get_balance(ticker_KRW)
                 if krw > 5000:
-                    upbit.buy_market_order(today_ticker, krw*0.9995) # 수수료 0.9995
-                    while state == 'done':
-                        state = upbit.get_order(get_order_uuid(ticker=today_ticker))['state']
-                        coin_volume = upbit.get_order(get_order_uuid(ticker=today_ticker))['volume']
-                    print('구매완료')
+                    try: 
+                        upbit.buy_market_order(today_ticker, krw*0.9995) # 수수료 0.9995
+                        logger.info('EVENT:구매 완료')
+                    except Exception as e:
+                        logger.info(e)
         else:
-            #btc = get_balance(ticker_ETC)
-            if coin_volume > 0.00008:
-                upbit.sell_market_order(today_ticker, coin_volume*0.9995)
-                print('판매완료')
-                coin_volume = 0
-                k_value = find_k(ticker=today_ticker)
-                
+            coin_volume, avg_buy_price = get_balance(today_ticker)
+            current_price = get_current_price(ticker=today_ticker)
+            if coin_volume > 0.00008 and avg_buy_price < current_price:
+                try: 
+                    upbit.sell_market_order(today_ticker, coin_volume*0.9995)
+                    logger.info('EVENT:판매 완료, 코인 갯수:',coin_volume)
+                    k_value = find_k(ticker=today_ticker)
+                except Exception as e:
+                    logger.info(e)
         time.sleep(3)
     except Exception as e:
-        print(e)
+        logger.info(e)
         time.sleep(3)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import configparser
+from datetime import date
+import datetime
 import pyupbit
 import numpy as np
 
@@ -17,54 +19,107 @@ ticker_ETC = "KRW-ETC" #이더리움 클래식
 ticker_QTUM = "KRW-QTUM" #퀀텀
 ticker_EOS = "KRW-EOS" #EOS
 
-#print(pyupbit.get_current_price(ticker_BTC))
+# #print(pyupbit.get_current_price(ticker_BTC))
+# upbit = pyupbit.Upbit(access_key, secret_key)
+# coin_volume = 0.13
+# krw = 10000
 
-df = pyupbit.get_ohlcv(ticker_EOS, interval = 'minutes240', count=200)
+# print(upbit.get_balances())
+# # upbit.sell_market_order(ticker_ETC, coin_volume*0.9995)
+# # print(upbit.get_balances())
 
-c = [-1-(i*3) for i in range(0,50)]
-df = df.iloc[c] # 12시간 간격으로 자름
+# def get_order_uuid(ticker):
+#     orders = upbit.get_order(ticker, state="done")
+#     print(orders)
+#     uuid = orders['uuid']
+#     return uuid
 
-df['range'] = (df['high'] - df['low']) * 0.5
-df['target'] = df['open'] + df['range'].shift(1)
+# get_order_uuid(ticker_ETC)
+#upbit.buy_market_order(ticker_ETC, krw*0.9995)
+#coin_volume = upbit.get_order(get_order_uuid(ticker=ticker_ETC))['volume']
+#print(coin_volume)
 
-df['ror'] = np.where(df['high'] > df['target'], df['close'] / df['target'],1)
+#df = pyupbit.get_ohlcv(ticker_ETC, interval = 'minutes240', count=200)
 
-ror = df['ror'].cumprod()[-2]
-print("누적수익률:", ror, sep=' ')
+# df = pyupbit.get_ohlcv(ticker_ETC, interval = 'minutes30', count=800)
 
-fee = 0.0032
-df['ror_fee'] = np.where(df['high'] > df['target'], df['close'] / df['target'] - fee, 1)
+# if df.index[-1].minute != 30:
+#   df = df.iloc[:-1] # 시간 정각은 제외한다
 
-ror_fee = df['ror_fee'].cumprod()[-2] # 하나 윗 컬럼
+# c = [-1-(i*8) for i in range(0,100)]
+# df = df.iloc[c] # 4시간 간격으로 자름
+
+
+# df['range'] = (df['high'] - df['low']) * 0.5
+# df['target'] = df['open'] + df['range'].shift(1)
+
+# df['ror'] = np.where(df['high'] > df['target'], df['close'] / df['target'],1)
+
+# ror = df['ror'].cumprod()[-2]
+# print("누적수익률:", ror, sep=' ')
+
+# fee = 0.0032
+# df['ror_fee'] = np.where(df['high'] > df['target'], df['close'] / df['target'] - fee, 1)
+
+# ror_fee = df['ror_fee'].cumprod()[-2] # 하나 윗 컬럼
   
-df['hpr'] = df['ror'].cumprod()
-df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+# df['hpr'] = df['ror'].cumprod()
+# df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
 
-print("MDD(%): ", df['dd'].max())
-print("누적수익률(fee고려):", ror_fee, sep=' ')
+# print("MDD(%): ", df['dd'].max())
+# print("누적수익률(fee고려):", ror_fee, sep=' ')
+
+#print(df.tail(10))
+#print(df.head(10))
 
 
 def get_ror(k, ticker):
-  df = pyupbit.get_ohlcv(ticker_EOS, interval = 'minutes240', count=200)
+  #df = pyupbit.get_ohlcv(ticker, interval = 'minutes240', count=200)
+  df = pyupbit.get_ohlcv(ticker, interval = 'minutes30', count=800)
+
+  if df.index[-1].minute != 30:
+    df = df.iloc[:-1] # 시간 정각은 제외한다
   
-  c = [-1-(i*3) for i in range(0,50)]
-  df = df.iloc[c] # 12시간 간격으로 자름
+  c = [-1-(i*8) for i in range(0,100)]
+  df = df.iloc[c] # 4시간 간격으로 자름
 
   df['range'] = (df['high'] - df['low']) * k
   df['target'] = df['open'] + df['range'].shift(1) 
 
   fee = 0.0032
-  df['ror'] = np.where(df['high'] > df['target'],df['close'] / df['target'] - fee,1)
+  df['ror'] = np.where(df['high'] > df['target'],df['close'] / df['target'] - fee, 1)
   ror = df['ror'].cumprod()[-2]
+
+  df['hpr'] = df['ror'].cumprod()
+  df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+
+  print("MDD(%): ", df['dd'].max())
+  #print("누적수익률(fee고려):", ror, sep=' ')
   
+  #print(df.tail(10))
+  #print(df.head(10))
+
   return ror
 
-r_list = []
-for k in np.arange(0.1, 1.0, 0.1):
-  ror = get_ror(k, ticker_QTUM)
-  print("누적수익률(fee고려) ==> k:{:.1}: {}".format(k,ror))
 
-  r_list.append(ror)
+# for k in np.arange(0.1, 1.0, 0.1):
+#   ror = get_ror(k, ticker_ETC)
+#   print("누적수익률(fee고려) ==> k:{:.1}: {}".format(k,ror))
 
-v = (int(r_list.index(max(r_list)))+1)/10
-print(type(v))
+#   r_list.append(ror)
+
+upbit = pyupbit.Upbit(access_key, secret_key)
+balances = upbit.get_balances()
+print(balances)
+
+tickers = [ticker_BTC, ticker_ETC, ticker_QTUM, ticker_EOS]
+t_list = []
+for ticker in tickers:
+  print('코인 이름:{}'.format(ticker))
+  r_list = []
+  for k in np.arange(0.1, 1.0, 0.1):
+    ror = get_ror(k, ticker)
+    print("누적수익률(fee고려) ==> k:{:.1}: {}".format(k,ror))
+
+    r_list.append(ror)
+  t_list.append(r_list)
